5174a40b000e7a7e86367008cd85d1f02125e4b4
ayafdez <aya@likhangmaya.com>
2026-02-15 20:59:17 -0500

    fixed page.tsx

--- next-site/app/page.tsx
+++ next-site/app/page.tsx
@@ -1,17 +1,11 @@
 import HomeHero from "@/components/home/HomeHero";
 import TrustSection from "@/components/home/TrustSection";
-import ServicesSection from "@/components/home/ServicesSection";
-import TestimonialsSection from "@/components/home/TestimonialsSection";
-import CTASection from "@/components/home/CTASection";
 
 export default function HomePage() {
   return (
     <main className="bg-white">
       <HomeHero />
       <TrustSection />
-      <ServicesSection />
-      <TestimonialsSection />
-      <CTASection />
     </main>
   );
 }
