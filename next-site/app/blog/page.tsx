9bb4d69de9a83833944969496d53cd77c32a9c5c
ayafdez <aya@likhangmaya.com>
2026-02-15 20:27:03 -0500

    delete

--- next-site/app/blog/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-// Redirect /blog to /learn/blog for consistency
-export default function BlogRedirect() {
-  redirect('/learn/blog');
-}
--- next-site/components/index.ts
+++
@@ -1,7 +0,0 @@
-// Layout Components
-export { Navbar } from './layout/Navbar';
-export { Footer } from './layout/Footer';
-export { GlobalCTA } from './layout/GlobalCTA';
-
-// UI Components
-export { SectionHeading } from './ui/SectionHeading';
